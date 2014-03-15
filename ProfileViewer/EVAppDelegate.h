a255753a7bd6d72d5b2f848a62657d6c3f4c610c
Eric Velazquez <evelazquez@parallel6.com>
2014-03-15 11:21:56 -0700

    Initial Commit

---
+++ ProfileViewer/EVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EVAppDelegate.h
+//  ProfileViewer
+//
+//  Created by Eric Velazquez on 3/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProfileViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProfileViewer
+//
+//  Created by Eric Velazquez on 3/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EVAppDelegate class]));
+    }
+}
